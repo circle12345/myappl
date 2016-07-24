aa471a7a514ee5b55064865abdc2f3208f4362bf
wangyuan5 <wangyuan5@le.com>
2016-07-24 22:47:18 +0800

    update branch dev

--- app/src/main/java/com/example/leeco/myapplication/MainActivity.java
+++ app/src/main/java/com/example/leeco/myapplication/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+//dev branch
         Toast.makeText(this,"toast for debug",Toast.LENGTH_SHORT).show();
 
     }
