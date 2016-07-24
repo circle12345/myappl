1f95ca45d3275092e6e1a9f7294dca530d094db6
wangyuan5 <wangyuan5@le.com>
2016-07-24 21:57:12 +0800

    change mainactivty

--- app/src/main/java/com/example/leeco/myapplication/MainActivity.java
+++ app/src/main/java/com/example/leeco/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.leeco.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,8 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //this for git
-        //this changes from github
-        //hahaha for hehehe
+
+
+        Toast.makeText(this,"toast for debug",Toast.LENGTH_SHORT).show();
+
     }
 }
